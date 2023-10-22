7d9c1851e8e6c2afef98359bcad61b2b1e56490e
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-10-22 08:18:00 +0700

    [REACTIVE] Initial project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   id("com.android.application")
   kotlin("android")
+  kotlin("plugin.serialization") version "1.9.10"
 }
 
 android {
@@ -37,4 +38,19 @@ dependencies {
   implementation("androidx.core:core-ktx:1.12.0")
   implementation("androidx.appcompat:appcompat:1.6.1")
   implementation("com.google.android.material:material:1.10.0")
+
+  // ReactiveX programming
+  implementation("io.reactivex.rxjava3:rxjava:3.1.8")
+  implementation("io.reactivex.rxjava3:rxkotlin:3.0.1")
+  implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
+
+  // Kotlin serialization
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+
+  // Network dependencies
+  implementation("com.squareup.retrofit2:retrofit:2.9.0")
+  implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+  implementation("com.squareup.retrofit2:adapter-rxjava3:2.9.0")
+  implementation("com.squareup.okhttp3:okhttp:4.12.0")
+  implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
--- app/src/main/kotlin/id/android/training/presentation/MainActivity.kt
+++ app/src/main/kotlin/id/android/training/presentation/MainActivity.kt
@@ -1,5 +1,6 @@
-package id.android.training
+package id.android.training.presentation
 
 import androidx.activity.ComponentActivity
+import id.android.training.R
 
 class MainActivity : ComponentActivity(R.layout.activity_main)
