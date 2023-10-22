42b1eb72e503bd13772177977d23a5b21229a267
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-10-22 15:18:31 +0700

    [REACTIVE] Implement view binding

--- app/src/main/kotlin/id/android/training/presentation/MainActivity.kt
+++ app/src/main/kotlin/id/android/training/presentation/MainActivity.kt
@@ -1,6 +1,17 @@
 package id.android.training.presentation
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
-import id.android.training.R
+import id.android.training.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity(R.layout.activity_main)
+class MainActivity : ComponentActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+  }
+}
